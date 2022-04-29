10d3d94656624e0030fbe3cf114a1b60f628c459
revol-xut <revol-xut@protonmail.com>
2022-04-29 20:19:44 +0200

    functioning deduplicater

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,22 @@ use std::fs::{File, OpenOptions};
 use actix_web::{web, App, HttpServer, Responder};
 use std::env;
 use csv::{WriterBuilder};
-use std::sync::{RwLock};
+use std::sync::{RwLock, Arc};
 
 async fn formatted(processor: web::Data<RwLock<Processor>>, telegram: web::Json<Telegram>) -> impl Responder {
 
     let telegram_hash = Processor::calculate_hash(&telegram).await;
-    println!("Hash {}", telegram_hash);
     let contained;
-    match processor.read() {
-        Ok(readable_processor) => {
-            println!("Start: index: {} field: {:?}", &readable_processor.iterator, &readable_processor.last_elements);
-            contained = readable_processor.last_elements.contains(&telegram_hash);
-        }
-        Err(_) => {
-            println!("ALARM!");
-            contained = false;
-        }
+     {
+        let readable_processor = processor.read().unwrap();
+        contained = readable_processor.last_elements.contains(&telegram_hash);
     }
-    println!("Contains {}", contained);
-    match processor.write() {
-        Ok(mut writeable_processor) => {
-            println!("End: end: {} field: {:?}", &writeable_processor.iterator, &writeable_processor.last_elements);
-            let index = writeable_processor.iterator;
-            writeable_processor.last_elements[index] = telegram_hash;
-            writeable_processor.iterator = (writeable_processor.iterator + 1) % DEPULICATION_BUFFER_SIZE;
-            println!("End: end: {} field: {:?}", &writeable_processor.iterator, &writeable_processor.last_elements);
-        }
-        Err(_) => {
-            println!("ALAARM!!");
-        }
-    };
+    {
+        let mut writeable_processor = processor.write().unwrap();
+        let index = writeable_processor.iterator;
+        writeable_processor.last_elements[index] = telegram_hash;
+        writeable_processor.iterator = (writeable_processor.iterator + 1) % DEPULICATION_BUFFER_SIZE;
+    }
 
     // do more processing
     if !contained {
@@ -63,14 +49,18 @@ async fn raw(telegram: web::Json<RawData>) -> impl Responder {
     web::Json(Response { success: true })
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    //let runtime: web::Data<Arc<RwLock<Processor>>>= web::Data::new(Arc::new(RwLock::new(Processor::new())));    
+    let data = web::Data::new(RwLock::new(Processor::new())); 
     println!("Starting Server ... ");
     let host = "127.0.0.1";
     let port = 8080;
     println!("Listening on: {}:{}", host, port);
-    HttpServer::new(|| App::new()
-                    .app_data(web::Data::new(RwLock::new(Processor::new())))
+    HttpServer::new(move || App::new()
+                    .app_data(data.clone())
                     .route("/formatted_telegram", web::post().to(formatted))
                     .route("/raw_telegram", web::post().to(raw))
 
