4986490c7bf59faec755fed35532d6a2fa06d2fb
revol-xut <revol-xut@protonmail.com>
2022-06-11 01:22:25 +0200

    reverted endpoint

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ async fn main() -> std::io::Result<()> {
                     .app_data(filter.clone())
                     .app_data(request_data.clone())
                     .app_data(database_struct.clone())
-                    .route("/telegram/r09/", web::post().to(formatted))
+                    .route("/formatted", web::post().to(formatted))
                     .route("/telegram/raw/", web::post().to(raw))
                     )
         .bind((host, port))?
