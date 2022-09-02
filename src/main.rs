6a32aa66b556b36ec765fad07fa0f3b01c8e487d
revol-xut <revol-xut@protonmail.com>
2022-09-03 00:08:08 +0200

    read db password directly from file

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ use std::sync::mpsc::{Receiver, SyncSender};
 use std::sync::{Mutex, Arc};
 use std::thread;
 use std::env;
+use std::fs;
 
 use dump_dvb::telegrams::{TelegramMetaInformation, r09::R09Telegram};
 
@@ -47,11 +48,13 @@ impl ApplicationState {
 pub fn create_db_pool() -> DbPool { 
     let default_postgres_host = String::from("localhost:5433");
     let default_postgres_port = String::from("5432");
-    let default_postgres_pw = String::from("default_pw");
+    let default_postgres_pw_path = String::from("/run/secrets/postgres_password");
+
+    let password_path = env::var("POSTGRES_PASSWORD_PATH").unwrap_or(default_postgres_pw_path.clone());       let password = fs::read_to_string(password_path).expect("cannot read password file!");
 
     let database_url = format!(
         "postgres://dvbdump:{}@{}:{}/dvbdump",
-        env::var("POSTGRES_DVBDUMP_PASSWORD").unwrap_or(default_postgres_pw.clone()),
+        password,
         env::var("POSTGRES_HOST").unwrap_or(default_postgres_host.clone()),
         env::var("POSTGRES_PORT").unwrap_or(default_postgres_port.clone())
     );
