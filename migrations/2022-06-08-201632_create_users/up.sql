4cf12b70c44eed9afa53627037853db201e62ee9
revol-xut <revol-xut@protonmail.com>
2022-10-21 12:31:15 +0200

    clean up

--- migrations/2022-06-08-201632_create_users/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id              UUID PRIMARY KEY,
-    name            TEXT NOT NULL,
-    email           TEXT NOT NULL,
-    password        VARCHAR(100) NOT NULL,
-    role            INT NOT NULL
-)
