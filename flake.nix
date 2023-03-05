5ed93a46016aa6caf0388d60a2020f0c3ef0f280
Grigory Shipunov <blame@oxapentane.com>
2023-03-05 02:36:29 +0100

    pin nixpkgs to stable

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     naersk = {
-      url = github:nix-community/naersk;
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     utils = {
-      url = github:numtide/flake-utils;
+      url = "github:numtide/flake-utils";
     };
   };
 
