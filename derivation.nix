d9fced8e1dcc299f262c2bcec773cc25da35b11a
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-05-19 10:07:12 +0200

    fix data-accumulator postgresql dependecy resolutions

--- derivation.nix
+++ derivation.nix
@@ -1,4 +1,5 @@
 { buildPackage
+, stdenv
 , lib
 , pkg-config
 , cmake
@@ -8,19 +9,43 @@
 , openssl
 }:
 
-buildPackage {
-  pname = "data-accumulator";
-  version = "0.5.0";
+let
+  data-accumulator = buildPackage {
+    pname = "data-accumulator";
+    version = "0.5.0";
 
-  src = ./.;
+    src = ./.;
 
-  cargoSha256 = lib.fakeSha256;
+    cargoSha256 = lib.fakeSha256;
 
-  nativeBuildInputs = [ pkg-config cmake ];
-  buildInputs = [ protobuf zlib postgresql_14 openssl ];
+    nativeBuildInputs = [ pkg-config cmake ];
+    buildInputs = [ protobuf zlib postgresql_14 openssl ];
 
-  meta = {
-    description = "Simple rust server which collects data from telegram stations";
-    homepage = "https://github.com/dump-dvb/data-accumulator";
+    meta = {
+      description = "Simple rust server which collects data from telegram stations";
+      homepage = "https://github.com/tlm-solutions/data-accumulator";
+    };
   };
+in
+stdenv.mkDerivation {
+  name = "data-accumulator-patchelf";
+  src = data-accumulator;
+
+  buildPhase = "";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/bin/data-accumulator $out/bin/data-accumulator
+    chmod +w $out/bin/data-accumulator
+  
+    patchelf --replace-needed libpq.so.5 ${postgresql_14.lib}/lib/libpq.so $out/bin/data-accumulator
+
+    # check if the patch succeded and exit if a depedency is not found
+    local patch_succeded
+
+    patch_succeded=$(ldd $out/bin/data-accumulator | grep "not found" | wc -l || true)
+    if [[ "$patch_succeded" -ne 0 ]]; then
+      echo "Patching failed" && exit 1
+    fi
+  '';
 }
